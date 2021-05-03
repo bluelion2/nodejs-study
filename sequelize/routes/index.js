077c2fe16d420d8365e8b6fed296e0b139b2dd37
seunghun Kang <iltk2000@gmail.com>
2021-05-03 23:56:58 +0900

    sequelize study

---
+++ sequelize/routes/index.js
@@ -0,0 +1,15 @@
+const express  =require('express')
+const User = require('../models/users')
+
+const router = express.Router()
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll()
+    res.render('sequelize', { users })
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
+})
+
+module.exports = router
